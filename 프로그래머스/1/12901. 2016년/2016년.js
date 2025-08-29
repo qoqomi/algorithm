0cc1a3fcb72520c02f76d844228dd20711ed130a
Seungyeon <99249544+qoqomi@users.noreply.github.com>
2025-08-29 20:15:26 +0900

    [level 1] Title: 2016년, Time: 0.07 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/12901. 2016년/2016년.js
+++ 프로그래머스/1/12901. 2016년/2016년.js
@@ -1,15 +1,4 @@
 function solution(a, b) {
-    // 윤년 : 2월 29일 존재 , 366일 
-    // 4,100,400으로 나눠지는 년도 
-    const weekDay = ["SUN","MON","TUE","WED","THU","FRI","SAT"]
-    const lastMonth = [31,29,31,30,31,30,31,31,30,31,30,31]
-    
-    let day = b + 4
-    
-    for(let i =0; i < a-1; ++i){
-        
-        day+= lastMonth[i]
-    }
-     
-    return weekDay[day%7]
+    var answer = '';
+    return answer;
 }
