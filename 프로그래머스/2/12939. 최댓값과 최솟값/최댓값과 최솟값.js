5ebc919f7480082afe6c4c7a74f88ba46ff89e7e
Seungyeon <99249544+qoqomi@users.noreply.github.com>
2025-09-02 21:02:56 +0900

    [level 2] Title: 최댓값과 최솟값, Time: 0.11 ms, Memory: 33.3 MB -BaekjoonHub

--- 프로그래머스/2/12939. 최댓값과 최솟값/최댓값과 최솟값.js
+++ 프로그래머스/2/12939. 최댓값과 최솟값/최댓값과 최솟값.js
@@ -1,4 +1,7 @@
 function solution(s) {
-    var answer = '';
-    return answer;
+    const arr = s.split(" ").map((item)=>Number(item))
+    let min = Math.min(...arr)
+    let max = Math.max(...arr)
+    
+    return `${min} ${max}`
 }
